085810aea7cd3f160018767e41408ec14cdba393
yadavmm30 <54914878+yadavmm30@users.noreply.github.com>
2019-11-12 10:13:17 -0500

    madels added

--- Code/Project_Code.py
+++ Code/Project_Code.py
@@ -1,5 +1,15 @@
 import pandas as pd
 import numpy as np
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import classification_report
+from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.preprocessing import StandardScaler
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import roc_auc_score
 
 
 # reading csv file data
@@ -13,13 +23,14 @@ df_cleaned = movie_data_orig.drop(["adult", "belongs_to_collection", "homepage",
                                    "original_title", "overview", "poster_path", "production_countries", "release_date",
                                    "spoken_languages", "status", "tagline", "video" ], axis=1)
 len(df_cleaned)       # 45466
-# print(df_cleaned.head(5))
 
+# print(df_cleaned.head(5))
 # taking into account only those movies where budget and/or revenue is greater than $100,000 (some of values in budget & revenue
 # columns are 0, 1, 2, 3 etc which do not make any sense
 
 df_cleaned.dtypes             # datatype of all columns in the dataframe; (budget is of "object" datatype instead of "int")
 df_cleaned.describe()           # giving summary statistics of columns with dtype = float64
+
 # budget column contains alpha-numeric characters, so need to fix it
 df_cleaned['budget'] = df_cleaned['budget'].str.extract('(\d+)', expand=False)   # removing all non-numeric values from budget column
 
@@ -35,6 +46,7 @@ df_cleaned = df_cleaned.loc[df_cleaned['revenue'] > 1000]      # subsetting df t
 df_cleaned["status"] = df_cleaned["revenue"]/df_cleaned["budget"]
 # df_cleaned.describe()
 # df_cleaned.dtypes
+
 
 # Our criteria for success is any value greater than 1 else flop
 df_cleaned["New_status"] = np.nan      # creating a new empty column
@@ -72,3 +84,139 @@ print(df_cleaned.columns)
 # there are many entries where the number of people who voted for a movie are 1, 2 , 3 etc
 df_cleaned = df_cleaned.loc[df_cleaned['vote_count'] > 100]      # subsetting df to only movies where atleast 100 people voted for a movie
 len(df_cleaned)      # 3763
+
+# Removing Duplicates
+df_cleaned.drop_duplicates(inplace = True)
+print(len(df_cleaned)) # 3763
+print(df_cleaned.columns)
+
+# Modeling
+#Decision Tree Gini
+# split the dataset into input and target variables
+X = df_cleaned.loc[:,['runtime','vote_average','Genre', 'Production Company']]
+y = df_cleaned.loc[:,['New_status']]
+
+# encloding the class with sklearn's LabelEncoder
+le = LabelEncoder()
+
+# fit and transform the class
+y = le.fit_transform(y)
+X = pd.get_dummies(X)
+
+# split the dataset into train and test
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=100)
+
+# perform training with giniIndex.
+# creating the classifier object
+clf_gini = DecisionTreeClassifier(criterion="gini", random_state=100, max_depth=3, min_samples_leaf=5)
+
+# performing training
+clf_gini.fit(X_train, y_train)
+
+# predicton on test using gini
+y_pred_gini = clf_gini.predict(X_test)
+
+print("Classification Report: ")
+print(classification_report(y_test,y_pred_gini))
+print("Accuracy : ", accuracy_score(y_test, y_pred_gini) * 100)
+
+#Decision Tree Entropy
+# perform training with Entropy.
+# creating the classifier object
+clf_entropy = DecisionTreeClassifier(criterion="entropy", random_state=100, max_depth=3, min_samples_leaf=5)
+
+# performing training
+clf_entropy.fit(X_train, y_train)
+
+# predicton on test using gini
+y_pred_entropy = clf_entropy.predict(X_test)
+
+print("Classification Report: ")
+print(classification_report(y_test,y_pred_entropy))
+print("Accuracy : ", accuracy_score(y_test, y_pred_entropy) * 100)
+
+#Applying SVM Classification
+# perform training
+# creating the classifier object
+clf = SVC(kernel="linear")
+
+# performing training
+clf.fit(X_train, y_train)
+
+# predicton on test
+y_pred_svm = clf.predict(X_test)
+
+# calculate metrics
+print("\n")
+
+print("Classification Report: ")
+print(classification_report(y_test,y_pred_svm))
+print("\n")
+
+print("Accuracy : ", accuracy_score(y_test, y_pred_svm) * 100)
+print("\n")
+
+#KNN
+# standardize the data
+stdsc = StandardScaler()
+
+stdsc.fit(X_train)
+
+X_train_std = stdsc.transform(X_train)
+X_test_std = stdsc.transform(X_test)
+
+# perform training
+# creating the classifier object
+clf_knn = KNeighborsClassifier(n_neighbors=3)
+
+# performing training
+clf_knn.fit(X_train_std, y_train)
+
+#%%-----------------------------------------------------------------------
+# make predictions
+
+# predicton on test
+y_pred_knn = clf.predict(X_test_std)
+
+#%%-----------------------------------------------------------------------
+# calculate metrics
+
+print("\n")
+print("Classification Report: ")
+print(classification_report(y_test,y_pred_knn))
+print("\n")
+
+
+print("Accuracy : ", accuracy_score(y_test, y_pred_knn) * 100)
+print("\n")
+
+#Naive Bayese
+# creating the classifier object
+clf_nb = GaussianNB()
+
+# performing training
+clf_nb.fit(X_train, y_train)
+
+#%%-----------------------------------------------------------------------
+# make predictions
+
+# predicton on test
+y_pred_nb = clf_nb.predict(X_test)
+
+y_pred_nb_score = clf_nb.predict_proba(X_test)
+
+#%%-----------------------------------------------------------------------
+# calculate metrics
+
+print("\n")
+
+print("Classification Report: ")
+print(classification_report(y_test,y_pred_nb))
+print("\n")
+
+
+print("Accuracy : ", accuracy_score(y_test, y_pred_nb) * 100)
+print("\n")
+
+print("ROC_AUC : ", roc_auc_score(y_test,y_pred_nb_score[:,1]) * 100)
+print("\n")
